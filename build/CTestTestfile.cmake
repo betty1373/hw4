8386bf51f38b41acfa8b702dcd01668662e75599
betty1373 <betty1373@mail.ru>
2021-05-09 22:48:45 +0300

    dd

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/btv/hw4
-# Build directory: /home/btv/hw4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(print_ip_tests "test_version")
-set_tests_properties(print_ip_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/btv/hw4/CMakeLists.txt;77;add_test;/home/btv/hw4/CMakeLists.txt;0;")
